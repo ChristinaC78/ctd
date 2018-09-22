f7c2044a396bb22a63eb37535f0575c26437661e
Christina Clark <divackc@gmail.com>
2018-09-22 15:43:10 +0000

    changes from class

--- week_one/user_input_program/user_input_statistics.rb
+++ week_one/user_input_program/user_input_statistics.rb
@@ -1,1 +1,22 @@
-# comment 
+#comment
+ages = ['32', '15', '40']
+puts ages
+puts ages [0]
+puts ages [1]
+puts ages [2]
+puts
+
+puts 'Hello, what\'s your name?'
+name = gets.chomp
+puts 'Hello, ' + name + '.'
+if name == 'Jaden'
+    puts 'What a lovely name!'
+else 
+    puts "What\'s good homie?!"
+end
+puts
+command = ''
+while command != 'bye'
+puts command
+command = gets.chomp
+end
