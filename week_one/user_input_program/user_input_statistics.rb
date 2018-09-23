eafaa2fa9b506315905e5dcc87e3caf5be245ac5
Christina Clark <divackc@gmail.com>
2018-09-23 22:35:47 +0000

    finally finished

--- week_one/user_input_program/user_input_statistics.rb
+++ week_one/user_input_program/user_input_statistics.rb
@@ -1,22 +1,35 @@
-#comment
-ages = ['32', '15', '40']
-puts ages
-puts ages [0]
-puts ages [1]
-puts ages [2]
-puts
+def is_float?(s)
+  if s.to_i.to_s == s
+    return true
+  end
+  s == s.to_f.to_s
+end
 
-puts 'Hello, what\'s your name?'
-name = gets.chomp
-puts 'Hello, ' + name + '.'
-if name == 'Jaden'
-    puts 'What a lovely name!'
-else 
-    puts "What\'s good homie?!"
+def main
+ stat= []
+
+ loop do
+     puts "Please enter a number"
+     value= gets.chomp
+
+     if value == ''
+
+         break
+     end
+     if is_float?(value)
+
+       stat << value
+     end
+
+   break if value == '' 
+ end
+ # output data
+
+ puts "The count equals " + stat.length.to_s
+ sum = 0.0
+ stat.each {|value| sum+=value.to_f }
+ puts "The sum equals " + sum.to_s
+ puts "The average equals " + (sum/stat.length).to_s
 end
-puts
-command = ''
-while command != 'bye'
-puts command
-command = gets.chomp
-end
+
+main if __FILE__ == $PROGRAM_NAME
