b92a879a806e557dd58f34c2989ac851551bb21d
Christina Clark <divackc@gmail.com>
2018-10-06 14:58:29 +0000

    change

--- week_three/object_oriented/animals.rb
+++ week_three/object_oriented/animals.rb
@@ -3,6 +3,14 @@ class Animals
     
     def land
         def birds
+            def flightless
+                def peacock
+                end
+            end
+            def flying
+                def robin
+                end
+            end
         end
         def mammals
         end
