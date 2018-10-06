3362c524d8cee1152651c2c56b8dfde94bab1229
Christina Clark <divackc@gmail.com>
2018-10-06 14:56:01 +0000

    practice

---
+++ week_three/object_oriented/animals.rb
@@ -0,0 +1,20 @@
+<LivingThings
+class Animals
+    
+    def land
+        def birds
+        end
+        def mammals
+        end
+    end
+    
+    def water
+        def fish
+        end
+        def amoeba
+        end
+        def cepholapods
+    end
+    end
+    
+    
